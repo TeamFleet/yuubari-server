7e5ab711479dc525155872e9110d7fc11c86785b
Diablohu <Diablohu@users.noreply.github.com>
2020-04-17 01:21:46 +0800

    New build 2020-04-16T17:21:46.064Z

--- server/index.js
+++ server/index.js
@@ -104,9 +104,9 @@ module.exports = {
 
 /***/ }),
 
-/***/ "./logs/tmp/config/portion.server.1587056543481.js":
+/***/ "./logs/tmp/config/portion.server.1587057646676.js":
 /*!*********************************************************!*\
-  !*** ./logs/tmp/config/portion.server.1587056543481.js ***!
+  !*** ./logs/tmp/config/portion.server.1587057646676.js ***!
   \*********************************************************/
 /*! exports provided: template, server */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
@@ -2526,7 +2526,8 @@ const context = {
   clearImmediate,
   // String,
   // Number,
-  Buffer // Boolean,
+  Buffer,
+  // Boolean,
   // Array,
   // Date,
   // Error,
@@ -2547,7 +2548,7 @@ const context = {
   // WeakSet,
   // Promise,
   // Symbol,
-
+  __dirname: eval('__dirname')
 };
 /**
  * 执行服务器端渲染 (Server-Side Rendering)
@@ -2987,7 +2988,7 @@ module.exports = KootReactRenderCache;
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _KOOT_PROJECT_CONFIG_PORTION_SERVER_PATHNAME___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! __KOOT_PROJECT_CONFIG_PORTION_SERVER_PATHNAME__ */ "./logs/tmp/config/portion.server.1587056543481.js");
+/* harmony import */ var _KOOT_PROJECT_CONFIG_PORTION_SERVER_PATHNAME___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! __KOOT_PROJECT_CONFIG_PORTION_SERVER_PATHNAME__ */ "./logs/tmp/config/portion.server.1587057646676.js");
 /* harmony import */ var _utils_get_pathname_dev_server_start__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../utils/get-pathname-dev-server-start */ "./node_modules/koot/utils/get-pathname-dev-server-start.js");
 /* harmony import */ var _utils_get_pathname_dev_server_start__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_utils_get_pathname_dev_server_start__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var _libs_create_koa_app__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../libs/create-koa-app */ "./node_modules/koot/libs/create-koa-app.js");
