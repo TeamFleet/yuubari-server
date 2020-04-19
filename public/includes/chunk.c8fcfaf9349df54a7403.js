254f0e2a4c36035e00a3f8147fd97002ef34cc06
Diablohu <Diablohu@users.noreply.github.com>
2020-04-20 04:03:46 +0800

    New build 2020-04-19T20:03:46.737Z

---
+++ public/includes/chunk.c8fcfaf9349df54a7403.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{1147:function(n,t,a){"use strict";a.r(t),function(n){var e=a(0),c=a.n(e),i=a(948);t.default=function(){return c.a.createElement(i.a,{title:"Naval Academy"})}}.call(this,a(39).default)}}]);
