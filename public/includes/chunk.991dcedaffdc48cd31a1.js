31c479cd4c5d6d6bb74d004f4e6c3b2156ecd999
Diablohu <Diablohu@users.noreply.github.com>
2020-07-23 00:42:57 +0800

    New build 2020-07-22T16:42:57.520Z

---
+++ public/includes/chunk.991dcedaffdc48cd31a1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1172:function(n,t,e){"use strict";e.r(t),function(n){var a=e(0),c=e.n(a),i=e(973);t.default=function(){return c.a.createElement(i.a,{title:"海軍兵学校"})}}.call(this,e(40).default)}}]);
